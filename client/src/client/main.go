bb316e8059d1b69b87cc815baeb9a4adb8db453d
Jeremy Huiskamp <jeremy.huiskamp@gmail.com>
2017-07-21 08:45:45 +0200

    basic prometheus instrumentation of client

--- client/src/client/main.go
+++ client/src/client/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
 
 func main() {
-	fmt.Println("World! Hello!")
+	addr := flag.String(
+		"listen-address",
+		":8079",
+		"The address to listen on for HTTP requests.")
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(*addr, nil))
 }
